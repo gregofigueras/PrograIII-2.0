2e58d443eb5d11fa8e0562e3aa9e95ce1bfe2ec7
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-05-30 19:44:42 -0300

    aa

--- src/modelo/Factura.java
+++ src/modelo/Factura.java
@@ -5,48 +5,48 @@ import java.util.GregorianCalendar;
 import interfaces.IAbonado;
 
 /**
- * @author 
- *Clase que representa una factura dentro del sistema
+ * @author Clase que representa una factura dentro del sistema
  */
-public class Factura implements Cloneable{ 
+public class Factura implements Cloneable {
 	private IAbonado abonado;
 	private GregorianCalendar fecha;
 	private double total;
+	private boolean pago;
 
 	/**
 	 * Constructor de la clase
-	 * @param decorator: decorator para realizar la factura
-	 * <b> pre: </b> decorator no debe ser nulo 
-	 * <b> post: </b> crea un objeto de tipo factura
+	 * 
+	 * @param decorator: decorator para realizar la factura <b> pre: </b> decorator
+	 *                   no debe ser nulo <b> post: </b> crea un objeto de tipo
+	 *                   factura
 	 */
 	public Factura(IAbonado abonado) {
 		super();
 		this.abonado = abonado;
 	}
-	
+
 	/**
-	 * metodo void que imprime la informacion de la factura de un abonado  
+	 * metodo void que imprime la informacion de la factura de un abonado
 	 */
 	public void getFactura() {
 		System.out.println(this.abonado.getNombre() + "     " + this.abonado.getDNI() + "\n");
 		this.abonado.imprimeServicios();
-		total=this.abonado.getCostoServicios();
-		this.fecha=abonado.getFecha();
+		total = this.abonado.getCostoServicios();
+		this.fecha = abonado.getFecha();
 	}
 
 	/**
-	 *metodo que clona la factura <b> 
-	 * puede retornar una excepcion 
+	 * metodo que clona la factura <b> puede retornar una excepcion
 	 */
 	@Override
 	public Object clone() throws CloneNotSupportedException {
-		Factura clonada= (Factura) super.clone();
-		clonada.abonado= (DecoratorPagos) abonado.clone();
+		Factura clonada = (Factura) super.clone();
+		clonada.abonado = (DecoratorPagos) abonado.clone();
 		return clonada;
 	}
-	
-	public void agregarServicio(String domicilio,Servicio s1) {
+
+	public void agregarServicio(String domicilio, Servicio s1) {
 		abonado.agregaServicio(domicilio, s1);
-		
+
 	}
 }
