dca3f09ae3e112fbb2badd6160b9b482b8327281
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-05-30 20:40:43 -0300

    Update GestorFacturacion.java

--- src/modelo/GestorFacturacion.java
+++ src/modelo/GestorFacturacion.java
@@ -2,26 +2,27 @@ package modelo;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+
 import interfaces.IAbonado;
 
 public class GestorFacturacion {
 	private IAbonado abonadoActual;
-	private ArrayList<IAbonado> listaAbonados;
 	private Factura factura;
 
 	public GestorFacturacion(ArrayList<IAbonado> listaAbonados) {
 		super();
 		this.listaAbonados = listaAbonados;
+
 	}
 
-	public void Facturacion () {
-		Iterator<IAbonado> it=listaAbonados.iterator();
+	public void Facturacion() {
+		Iterator<IAbonado> it = listaAbonados.iterator();
 		while (it.hasNext()) {
-		abonadoActual=it.next();
-		factura= new Factura (abonadoActual);
-		factura.getFactura();
-		abonadoActual.AgregarFactura(factura);
+			abonadoActual = it.next();
+			factura = new Factura(abonadoActual);
+			factura.getFactura();
+			abonadoActual.AgregarFactura(factura);
 		}
 	}
-	
+
 }
