411082f4c4a5b3011d4c1d05bd5a49ff89db3be1
gregofigueras <65877630+gregofigueras@users.noreply.github.com>
2023-06-09 14:07:07 -0300

    ventanaprincipal

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -24,6 +24,11 @@ import javax.swing.border.LineBorder;
 import controlador.Controlador;
 import interfaces.IAbonado;
 import modelo.Tecnico;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -55,6 +60,30 @@ public class VentanaPrincipal extends JFrame {
 	private JButton btnAgregar;
 	private DefaultListModel<IAbonado> modeloListaAbonado;
 	private DefaultListModel<Tecnico> modeloListaTecnico;
+	private JTextField nombre;
+	private JLabel lblNewLabel_1;
+	private JTextField dni;
+	private JPanel panelTexto1;
+	private JPanel panelDNI;
+	private JLabel lblNewLabel;
+	private JPanel panelNombre;
+	private JRadioButton rdbtnJuridico;
+	private JPanel panelTexto2;
+	private JPanel panelBotonesTipo;
+	private JPanel panelBotonesPago;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JPanel panelEliminarAbonado;
+	private JRadioButton rdbtnFisico;
+	private JRadioButton rdbtnCheque;
+	private JRadioButton rdbtnEfectivo;
+	private JRadioButton rdbtnCredito;
+	private final ButtonGroup buttonGroup_1 = new ButtonGroup();
+	private JButton btnEliminarAbonado;
+	private JButton btnAgregarAbonado;
+	private JPanel panelAgregarAbonado;
+	private JPanel panelSolicitar;
+	private JPanel panelEliminarTecnico;
+	private JPanel panelAgregarTecnico;
 
 	/**
 	 * Launch the application.
@@ -77,7 +106,7 @@ public class VentanaPrincipal extends JFrame {
 	 */
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 680, 422);
+		setBounds(100, 100, 787, 575);
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -135,8 +164,77 @@ public class VentanaPrincipal extends JFrame {
 		this.panelDarDeBajaS.add(this.btnDarDeBajaS);
 		
 		this.panelC2 = new JPanel();
-		this.panelC2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		this.panelC2.setBorder(new TitledBorder(null, "Crear Abonado", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		this.panelCentral.add(this.panelC2);
+		this.panelC2.setLayout(new GridLayout(4, 2, 0, 0));
+		
+		this.panelNombre = new JPanel();
+		this.panelC2.add(this.panelNombre);
+		
+		this.lblNewLabel = new JLabel("Nombre:");
+		this.panelNombre.add(this.lblNewLabel);
+		this.lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		
+		this.panelTexto1 = new JPanel();
+		this.panelC2.add(this.panelTexto1);
+		
+		this.nombre = new JTextField();
+		this.panelTexto1.add(this.nombre);
+		this.nombre.setColumns(10);
+		
+		this.panelDNI = new JPanel();
+		this.panelC2.add(this.panelDNI);
+		
+		this.lblNewLabel_1 = new JLabel("DNI:");
+		this.panelDNI.add(this.lblNewLabel_1);
+		this.lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		
+		this.panelTexto2 = new JPanel();
+		this.panelC2.add(this.panelTexto2);
+		
+		this.dni = new JTextField();
+		this.panelTexto2.add(this.dni);
+		this.dni.setColumns(10);
+		
+		this.panelBotonesTipo = new JPanel();
+		this.panelC2.add(this.panelBotonesTipo);
+		this.panelBotonesTipo.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		this.rdbtnJuridico = new JRadioButton("Juridico");
+		buttonGroup.add(this.rdbtnJuridico);
+		this.panelBotonesTipo.add(this.rdbtnJuridico);
+		
+		this.rdbtnFisico = new JRadioButton("Fisico");
+		buttonGroup.add(this.rdbtnFisico);
+		this.panelBotonesTipo.add(this.rdbtnFisico);
+		
+		this.panelBotonesPago = new JPanel();
+		this.panelC2.add(this.panelBotonesPago);
+		this.panelBotonesPago.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		this.rdbtnCheque = new JRadioButton("Cheque");
+		buttonGroup_1.add(this.rdbtnCheque);
+		this.panelBotonesPago.add(this.rdbtnCheque);
+		
+		this.rdbtnEfectivo = new JRadioButton("Efectivo");
+		buttonGroup_1.add(this.rdbtnEfectivo);
+		this.panelBotonesPago.add(this.rdbtnEfectivo);
+		
+		this.rdbtnCredito = new JRadioButton("Credito");
+		buttonGroup_1.add(this.rdbtnCredito);
+		this.panelBotonesPago.add(this.rdbtnCredito);
+		
+		this.panelEliminarAbonado = new JPanel();
+		this.panelC2.add(this.panelEliminarAbonado);
+		
+		this.btnEliminarAbonado = new JButton("Eliminar");
+		this.panelEliminarAbonado.add(this.btnEliminarAbonado);
+		
+		this.panelAgregarAbonado = new JPanel();
+		this.panelC2.add(this.panelAgregarAbonado);
+		
+		this.btnAgregarAbonado = new JButton("Agregar");
+		this.panelAgregarAbonado.add(this.btnAgregarAbonado);
 		
 		this.panelC3 = new JPanel();
 		this.panelC3.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -145,21 +243,30 @@ public class VentanaPrincipal extends JFrame {
 		
 		this.panelTecnicos = new JPanel();
 		this.panelC3.add(this.panelTecnicos, BorderLayout.SOUTH);		
+		this.panelTecnicos.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		this.panelSolicitar = new JPanel();
+		this.panelTecnicos.add(this.panelSolicitar);
 		
 		this.btnSolicitar = new JButton("Solicitar");
+		this.panelSolicitar.add(this.btnSolicitar);
 		this.btnSolicitar.addActionListener(Controlador.getInstance());
 		
+		this.panelEliminarTecnico = new JPanel();
+		this.panelTecnicos.add(this.panelEliminarTecnico);
+		
 		this.btnDarDeBaja = new JButton("Eliminar");
+		this.panelEliminarTecnico.add(this.btnDarDeBaja);
 		this.btnDarDeBaja.addActionListener(Controlador.getInstance());
 		this.btnDarDeBaja.setHorizontalAlignment(SwingConstants.LEFT);
 		
+		this.panelAgregarTecnico = new JPanel();
+		this.panelTecnicos.add(this.panelAgregarTecnico);
+		
 		this.btnAgregar = new JButton("Agregar");
+		this.panelAgregarTecnico.add(this.btnAgregar);
 		this.btnAgregar.addActionListener(Controlador.getInstance());
 		this.btnAgregar.setHorizontalAlignment(SwingConstants.RIGHT);
-		this.panelTecnicos.setLayout(new BoxLayout(this.panelTecnicos, BoxLayout.X_AXIS));
-		this.panelTecnicos.add(this.btnSolicitar);
-		this.panelTecnicos.add(this.btnDarDeBaja);
-		this.panelTecnicos.add(this.btnAgregar);
 		
 		this.lblAccionesTecnico = new JLabel("Acciones de tecnico");
 		this.panelC3.add(this.lblAccionesTecnico, BorderLayout.NORTH);
